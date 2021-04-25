09f01009c9affee68776de8949aa479eeff1c20a
asumanaslis <61350777+asumanaslis@users.noreply.github.com>
2021-04-25 16:36:39 +0300

    notifications screen added

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,10 @@ import 'package:crowd/routes/login.dart';
 import 'package:crowd/routes/signup.dart';
 import 'package:crowd/routes/search.dart';
 import 'package:crowd/routes/bottomTabBar.dart';
+import 'package:crowd/routes/notifications.dart';
 
 void main() => runApp(MaterialApp(
-      initialRoute: "/search",
+      initialRoute: "/notifications",
       routes: {
         '/walkThrough': (context) => WalkThrough(),
         '/welcome': (context) => Welcome(),
@@ -16,5 +17,6 @@ void main() => runApp(MaterialApp(
         '/signup': (context) => SignUp(),
         '/search': (context) => Search(),
         '/bottomTabBar': (context) => BottomTabBar(),
+        '/notifications': (context) => Notifications(),
        },
     ));
